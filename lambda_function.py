7756fcbeeac498909853a495f67e7fe4b3c6bcac
Ariel Roy <ariel@hyfi.io>
2024-03-05 13:43:07 -0500

    updating lambda as a test

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,3 @@
 # A simple test lambda function that returns a string
 def lambda_handler(event, context):
-    return "Hello from Lambda!"
+    return "Hello from Lambda! We've successfully updated things!"
